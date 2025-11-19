0620a3198455d703b0ab6626e33abe799c4c7085
pengbo <pbstar@sina.cn>
2025-11-19 23:29:26 +0800

    1

--- src/db/config.js
+++ src/db/config.js
@@ -1,14 +1,7 @@
-// export default {
-//   host: 'localhost',
-//   port: 3306,
-//   user: 'root',
-//   password: 'your_password',
-//   database: 'pbstar_admin',
-// }
 export default {
-  host: "152.136.96.92",
+  host: 'localhost',
   port: 3306,
-  user: "pbstar-admin",
-  password: "271915",
-  database: "pbstar-admin",
-};
+  user: 'root',
+  password: 'your_password',
+  database: 'pbstar_admin',
+}
